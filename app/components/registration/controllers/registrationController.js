f4b78acc423d804dd46cda01a2385265647fdf77
oleksandr_bondar <oleksandr_bondar@epam.com>
2016-11-29 12:29:02 +0100

    Initial commit

---
+++ app/components/registration/controllers/registrationController.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+    angular.module('BAAS.Registration')
+        .controller('RegistrationController', RegistrationController);
+
+    function RegistrationController() {
+        var controller = this;
+    };
+})();
